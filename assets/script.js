0bbcf56c0278d59ed8716ea2bb4fd2955d5a586b
tiedanGH <2295824927@qq.com>
2025-12-17 00:45:24 -0500

    homepage: move script to separate file

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// 如果不是在 GitHub 域名访问时，修改 info 文本
+(function() {
+    const githubDomains = ["github.tiedan.site", "page.tiedan.site", "tiedangh.github.io"];
+    const host = window.location.hostname.toLowerCase();
+    const isGithub = githubDomains.some(domain => host === domain || host.endsWith("." + domain));
+
+    if (!isGithub) {
+        window.addEventListener("DOMContentLoaded", function() {
+            const info = document.querySelector(".info");
+            if (info) {
+                info.innerHTML = 'Github Page：<a href="https://page.tiedan.site">https://page.tiedan.site</a>';
+            }
+        });
+    }
+})();
